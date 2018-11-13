1c71fa936a6146937c7391fdf7c79ab3e7023c78
shahin1290 <s.patowary@yahoo.com>
2018-11-13 11:24:21 +0100

    Adds partials for about-me section and puts some content and my portfolio image

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 As a portfolio owner
 In order to give a concise information about me to the visitor 
 I would like to create a homepage with my name and small description about me
+
+As a visitor
+In order to get more information about the portfolio owner
+I would like to see an about page with some informations and an image
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,11 +1,21 @@
 describe 'Index Page', type: :feature do
-    it 'displays project list' do
+    it 'displays homepage information' do
         visit '/'
 
         expect(page).to have_css '#info'
         within '#info' do
             expect(page).to have_content 'MD SHAHIN PATOWARY'
             expect(page).to have_content 'Junior Full Stack Web Developer'
+        end
+    end
+
+    it 'displays more informations about me and my image' do
+        visit '/'
+
+        expect(page).to have_css '#about-me'
+        expect(page).to have_css '#my-image'
+        within '#about-me' do
+            expect(page).to have_content 'About Me'
         end
     end
 end
