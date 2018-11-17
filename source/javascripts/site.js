ae284a3f6edfd9e7a0ae161c6f4a36182c1ef36e
shahin1290 <s.patowary@yahoo.com>
2018-11-17 13:44:26 +0100

    Adds js to give header typewriter effect

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+
+document.addEventListener('DOMContentLoaded', function(){
+    var i=0,txt;
+    txt = "Junior Full Stack Web Developer"
+    
+    function typing(){
+        if(i<txt.length){
+            document.querySelector('#info p').innerHTML += txt.charAt(i);
+
+            i++
+            setTimeout(typing, 50)
+        }
+    }
+    typing()
+})
