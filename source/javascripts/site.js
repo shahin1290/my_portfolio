e5b8065ca30d956e951cd53b1b635985902106f8
shahin1290 <s.patowary@yahoo.com>
2018-11-17 19:49:29 +0100

    adds effect by js to navbar when scrolls

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function(){
+
+    //TYPEWRITER EFFECT
     var i=0,txt;
     txt = "Junior Full Stack Web Developer"
     
@@ -12,4 +14,30 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
     typing()
+
+
+    //SCROLLING EFFECT
+
+    // var nav = document.querySelector('.nav-bar')
+    // window.onscroll = function(){
+    //     if(window.pageXOffset > 100){
+    //         nav.style.background = '#007bff'
+    //     }else {
+    //         nav.style.background = 'transparent'
+    //     }
+    // }
+
+    function Scroll(){
+        var top = document.querySelector('.nav-bar')
+        if(window.pageYOffset > 917){
+            top.style.background = '#576658'
+        }else {
+            
+            top.style.background = '#3C443A'
+        }
+
+    }
+
+    window.addEventListener("scroll", Scroll)
+   
 })
